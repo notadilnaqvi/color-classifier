cd2b9245e164e7d8eeb7d4bfd005ed4a70a43bed
sahnbk <sahnbk@gmail.com>
2019-07-29 11:59:02 +0500

    Updated app.py

--- app.py
+++ app.py
@@ -9,22 +9,32 @@ all_data = pandas.read_csv('dataset.csv')
 all_features = all_data.drop('label', axis=1)
 all_lables = all_data['label']
 
-X_train, X_test, y_train, y_test = train_test_split(all_features, all_lables, stratify = all_lables, train_size = 0.80)
+# X_train, X_test, y_train, y_test
+train_features, test_features, train_labels, test_labels = train_test_split(all_features, all_lables, stratify = all_lables, train_size = 0.75, )
+# print(type(X_test))
 
-scaler = StandardScaler()
-scaler.fit(X_train)
+# scaler = StandardScaler()
+# scaler.fit(X_train)
+#
+# StandardScaler(copy=True, with_mean=True, with_std=True)
+#
+# X_train = scaler.transform(X_train)
+# X_test = scaler.transform(X_test)
+# rgb = [[145, 98, 3], [100, 100, 100]]
+# df = pandas.DataFrame(rgb, columns = ['r', 'g', 'b'])
+mlp = MLPClassifier(hidden_layer_sizes=(5), max_iter=500)
+mlp.fit(train_features, train_labels)
+# score = mlp.score(test_features, test_labels)
+# print(score)
+predictions = mlp.predict(test_features)
+# print(predictions)
 
-StandardScaler(copy=True, with_mean=True, with_std=True)
 
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
 
-mlp = MLPClassifier(hidden_layer_sizes=(9,9,9),max_iter=5000)
-mlp.fit(X_train,y_train)
-predictions = mlp.predict(X_test)
+print(confusion_matrix(test_labels,predictions))
+print(classification_report(test_labels,predictions))
 
-print(confusion_matrix(y_test,predictions))
-print(classification_report(y_test,predictions))
+
 # print(all_lables.shape)
 # top = all_lables.head()
 # print(top)
